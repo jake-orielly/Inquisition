6f330560a8178acc1a5c3938e6a9c7cc855eb256
jake-orielly <jake.orielly@gmail.com>
2018-03-03 18:09:15 -0500

    Working twoards bug fixes.

--- enemies.js
+++ enemies.js
@@ -1,31 +1,29 @@
 function frothingHeretic() {
-    return new Character(10,13,1,dagger([rusty]),"the frothing heretic",hisHers(),"frothingHeretic");
+    return new Character(10,13,1,dagger([rusty]),"the frothing heretic",hisHers(),"enemy","frothingHeretic");
 }
 
 function ravenousGlutton() {
-    return new Character(45,15,2,cleaver(),"the ravenous glutton",hisHers(),"ravenousGlutton");
+    return new Character(45,15,2,cleaver(),"the ravenous glutton",hisHers(),"enemy","ravenousGlutton");
 }
 
 function fallenKnight() {
-    return new Character(25,20,3,shortSword(),"the fallen knight",hisHers(),"fallenKnight",[secondWind]);
+    return new Character(25,20,3,shortSword(),"the fallen knight",hisHers(),"enemy","fallenKnight",[secondWind]);
 }
 
 function createPlayer() {
-    return new Character(20,13,3,morningStar,"Jake",["him","his"],"player",[secondWind,doubleEdge,flagellate,woundedFury,retaliation]);
+    var player = new Character(20,13,3,morningStar,"Jake",["him","his"],"player","player",[secondWind,doubleEdge,flagellate,woundedFury,retaliation]);
+    initCharacter(player);
+    return player;
 }
 
-/*var player = {hp:20,maxHP:20,ac:13,attack:3,currCools:makeCooldowns(),name:"Jake",perPronoun:"him",possPronoun:"his",
-            weapon:morningStar(),
-            abilities:[secondWind,doubleEdge,flagellate,woundedFury,retaliation],charType:"player",damageTriggers:[],hpTriggers:[],buffs:{damage:[],critChance:[]}};*/
-
-var Character = function(maxHP,ac,attack,weapon,name,pronounSet,image,abilities = [],damageTriggers = [],hpTriggers = [],buffs = {}) {
+var Character = function(maxHP,ac,attack,weapon,name,pronounSet,charType,image,abilities = [],damageTriggers = [],hpTriggers = [],buffs = {}) {
     this.hp = maxHP;
     this.maxHP = maxHP;
     this.ac = ac;
     this.attack = attack;
     this.weapon = weapon;
     this.name = name;
-    this.charType = "enemy";
+    this.charType = charType;
     this.perPronoun = pronounSet[0];
     this.possPronoun = pronounSet[1];
     this.image = image;
@@ -33,6 +31,27 @@ var Character = function(maxHP,ac,attack,weapon,name,pronounSet,image,abilities 
     this.damageTriggers = damageTriggers;
     this.hpTriggers = hpTriggers;
     this.buffs = buffs;
+}
+
+function initCharacter(given) {
+    var curr;
+    var curr2;
+
+    for (var i = 0; i < given.abilities.length; i++) { //ToDo clean up var names and make that else readable . . . somehow
+        if (given.abilities[i].categories){
+            curr = given.abilities[i].categories;
+            for (var j = 0; j < Object.keys(curr).length; j++) {
+                curr2 = Object.keys(curr)[j];
+                if (curr2 != "buffs")
+                    given[curr2].push(curr[curr2]);
+                else {//Push the buff onto the right buff sub category 
+                    if (!given[curr2][Object.keys(curr[curr2])])
+                        given[curr2][Object.keys(curr[curr2])] = [];
+                    given[curr2][Object.keys(curr[curr2])].push(curr[curr2][Object.keys(curr[curr2])]);
+                }
+            }
+        }
+    }
 }
 
 function hisHers() {
