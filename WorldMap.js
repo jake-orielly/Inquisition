01f9207b018753c70ebf64aedbd156ed859535b4
jake-orielly <jake.orielly@gmail.com>
2018-04-13 23:43:05 -0400

    Can now sell items to shop.

--- WorldMap.js
+++ WorldMap.js
@@ -1,15 +1,20 @@
 var board = "";
-var boardCols = 11; 
-var boardRows = 11;
+var boardCols = 15; 
+var boardRows = 10;
 var playerHTML = "<img class='tileItem' id='playerHTML' src='art/soldier.png'>";
 var villageHTML = "<img class='village tileItem' id='villageHTML' src='art/village.png'>";
 var playerX = 2;
 var playerY = 2;
 var nextEncounter = 50;
 var inCombat = false;
+var inventoryMax = 15;
 
-var meat = new Item("Meat",2);
+var meat = new Item("meat",false,2);
+var axe = new Item("axe",false,2);
+var gold = new Item("gold",true,1);
+
 var inventory = [];
+var shopInventory = [new InventoryItem(axe,1)];
 
 $(".inquisition").hide();
 
@@ -50,8 +55,10 @@ function movePlayer(x,y) {
 
         if (tileType == "empty")
             nextEncounter -= 5;
-        else if (tileType == "village")
-            console.log(4);
+        else if (tileType == "village") {
+            showInventory();
+            showShop();
+        }
 
         $("#playerHTML").remove();
         newLocation.append(playerHTML);
@@ -62,6 +69,33 @@ function movePlayer(x,y) {
 }
 
 function toggleInventory() {
+    if ($("#inventory").is(":visible"))
+        $("#inventory").hide();
+    else 
+        showInventory();
+}
+
+function showShop() {
+    var result, curr;
+    
+    for (var i = 0; i < 5; i++) {
+        result += "<tr>";
+        for (var j = 0; j < 5; j++) {
+            curr = i*5 + j+1;
+            result += "<td>";
+            result += "<img class='inventorySlot' src='art/inventorySlot.png'>";
+            if(curr < shopInventory.length+1) {
+                result += "<img class='inventoryItem' src='art/" + shopInventory[curr-1].item.name + ".png'>";
+            }
+            result += "</td>";
+        }
+        result += "</tr>";
+    }
+    $("#shopTable").html(result);
+    $("#shop").show();
+}
+
+function showInventory() {
     var result;
     var curr;
     
@@ -70,16 +104,28 @@ function toggleInventory() {
         for (var j = 0; j < 3; j++) {
             curr = i*3 + j+1;
             result += "<td>";
+            result += "<img class='inventorySlot' src='art/inventorySlot.png'>";
             if(curr < inventory.length+1) {
-                result += "<img class='inventoryItem' src='art/" + "meat" + ".jpg'>";
-                console.log(1);
+                result += "<img class='inventoryItem' onclick='itemClick(" + (curr-1) + ")' src='art/" + inventory[curr-1].item.name + ".png'>";
+                if (inventory[curr-1].amount > 1)
+                    result += "<p class='inventoryItemAmount'>" + inventory[curr-1].amount + "</p>";
             }
             result += "</td>";
         }
         result += "</tr>";
     }
     $("#inventoryTable").html(result);
-    $("#inventory").toggle();
+    $("#inventory").show();
+}
+
+function itemClick(given) {
+    if ($("#shop").is(":visible")) {
+        if(inventory[given].item.name != "gold") {
+            addItem(gold,(inventory[given].item.value*inventory[given].amount));
+            removeItem(inventory[given].item,inventory[given].amount);
+            showInventory();
+        }   
+    }
 }
 
 document.addEventListener('keydown', function(event) {
@@ -99,12 +145,16 @@ function loot(given) {
     var curr;
     for (var i = 0; i < given.loot.length; i++) {
         curr = given.loot[i];
-        if (percentile() <= curr.odds) {
-            if (Array.isArray(curr.amount))
-                addItem(curr.item,rangeVal(curr.amount));
-            else 
-                addItem(curr.item,curr.amount);
+        if (inventory.length < inventoryMax) {
+            if (percentile() <= curr.odds) {
+                if (Array.isArray(curr.amount))
+                    addItem(curr.item,rangeVal(curr.amount));
+                else 
+                    addItem(curr.item,curr.amount);
+            }
         }
+        else 
+            alert("Inventory Full");
     }
 }
 
@@ -112,10 +162,20 @@ function percentile() {
     return parseInt(Math.random()*100+1);
 }
 
-function Item(name,value,recipe) {
+function Item(name,stackable,value,recipe) {
     this.name = name;
+    this.stackable = stackable;
     this.value = value;
     this.recipe = recipe;
+    
+    this.getName = function() {
+        var result = name;
+        result = result.charAt(0).toUpperCase() + result.substr(1);
+        for (var i = 0; i < result.length; i++)
+            if (result.charAt(i) == "_")
+                result = result.substr(0,i) + " " + result.charAt(i+1).toUpperCase() + result.substr(i+2);
+        return result;
+    }
 }
 
 function InventoryItem(item,amount) {
@@ -125,13 +185,33 @@ function InventoryItem(item,amount) {
 
 function addItem (item,amount) {
     var newAmount;
-    for (var i = 0; i < inventory.length; i++) {
-        if (item == inventory[i].item) {
-            newAmount = inventory[i].amount + amount;
-            inventory[i] = new InventoryItem(item, newAmount);
-            return;
+    if (item.stackable) {
+        for (var i = 0; i < inventory.length; i++) {
+            if (item == inventory[i].item) {
+                newAmount = inventory[i].amount + amount;
+                inventory[i] = new InventoryItem(item, newAmount);
+                return;
+            }
         }
     }
 
     inventory.push(new InventoryItem(item,amount));
+}
+
+function removeItem (item,amount) {
+    for (var i = 0; i < inventory.length; i++) {
+        if (item == inventory[i].item) {
+            if (inventory[i].amount >= amount) {
+                inventory[i].amount -= amount;
+                
+                if (inventory[i].amount == 0)
+                    inventory.splice(i,1);
+            }
+            else
+                alert("Error 2: Tried to remove more of item than is in inventory.")
+            return;
+        }
+    }
+
+    alert("Error 1: Tried to remove item not in inventory.")
 }
